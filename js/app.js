0e664bb7579367090ac9c8da0eb39b0fe38a121e
wolfvain305 <dwolf0305@gmail.com>
2024-06-11 22:03:20 -0400

    Constants and Event Listeners

---
+++ js/app.js
@@ -0,0 +1,36 @@
+// Constants
+    //Canvas
+        let canvas = document.getElementById("canvas")
+        let ctx = canvas.getContext('2d')
+
+    //Buttons
+        let startBtn = document.getElementById('startBtn')
+        let pauseBtn = document.getElementById('pauseBtn')
+        let restartBtn = document.getElementById('restartBtn')
+        let EasyBtn = document.getElementById('restartBtn')
+        let MediumBtn = document.getElementById('restartBtn')
+        let HardBtn = document.getElementById('restartBtn')
+        let animationId
+        let gameRunning = false
+
+// Setting the Event Listeners
+    startBtn.addEventListener('click', function() {
+        if (!gameRunning) {
+            gameRunning = true
+            loop()
+        }
+    })
+
+    pauseBtn.addEventListener('click', function() {
+        gameRunning = false
+        cancelAnimationFrame(animationId)
+    })
+
+    restartBtn.addEventListener('click', function() {
+        document.location.reload()
+    })
+
+
+    // EasyBtn.addEventListener('click')
+    // MediumBtn.addEventListener('click')
+    // HardBtn.addEventListener('click')
