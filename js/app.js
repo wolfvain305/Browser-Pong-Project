52487d74a5d5243a29c3d5f403cac573697ef8ef
wolfvain305 <dwolf0305@gmail.com>
2024-06-14 17:54:49 -0400

    Difficulty is done

--- js/app.js
+++ js/app.js
@@ -7,20 +7,64 @@
         const startBtn = document.getElementById('startBtn')
         const pauseBtn = document.getElementById('pauseBtn')
         const restartBtn = document.getElementById('restartBtn')
-        // let EasyBtn = document.getElementById('restartBtn')
-        // let MediumBtn = document.getElementById('restartBtn')
-        // let HardBtn = document.getElementById('restartBtn')
         let animationId
         let gameRunning = false
-
-    // Message Board
+        
+        // Message Board
         const messageEl = document.getElementById('message');
-
-    //Key Presses
+        
+        //Key Presses
         let upPressed = false
         let downPressed = false
         let wPressed = false
         let sPressed = false
+    
+    let easy = document.getElementsByName('easyBtn')
+    let medium = document.getElementsByName('mediumBtn')
+    let hard = document.getElementsByName('hardBtn')
+    
+    
+    //Game Items
+    // Ball properties
+    let ballRadius = 10
+    let ballX = canvas.width / 2
+    let ballY = canvas.height / 2
+    let ballSpeedX = 2.5
+    let ballSpeedY = 2.5
+    
+    // Paddle properties
+    let paddleHeight = 100
+    let paddleWidth = 10
+    let leftPaddle = canvas.height / 2 - paddleHeight / 2
+    let rightPaddle = canvas.height / 2 - paddleHeight / 2
+    let paddleSpeed = 7.5
+    
+    // Score boarding
+    let leftPlayerScore = 0
+    let rightPlayerScore = 0
+    let winningScore = 2
+    
+    // Difficulty
+    let selectDifficulty = document.getElementById('difficultySelect')
+    const difficultyLevels = {
+        easy: {
+            ballSpeedX: 2.5,
+            ballSpeedY: 2.5,
+            paddleSpeed: 5
+        },
+        medium: {
+            ballSpeedX: 3.5,
+            ballSpeedY: 3.5,
+            paddleSpeed: 7.5
+        },
+        hard: {
+            ballSpeedX: 5,
+            ballSpeedY: 5,
+            paddleSpeed: 10
+        },
+    }
+    
+    let currentDifficulty = difficultyLevels.easy
 
 // Setting the Event Listeners
     startBtn.addEventListener('click', function() {
@@ -29,97 +73,87 @@
             loop()
         }
     })
-
+    
     pauseBtn.addEventListener('click', function() {
         gameRunning = false
         cancelAnimationFrame(animationId)
     })
-
+    
     restartBtn.addEventListener('click', function() {
         document.location.reload()
     })
-
+    
     document.addEventListener("keydown", keyDownPress)
     document.addEventListener("keyup", keyUpPress)
-
+    
     // Loads the game when first opening page
     addEventListener("load", (event) => {
         draw()
     })
 
-    // EasyBtn.addEventListener('click')
-    // MediumBtn.addEventListener('click')
-    // HardBtn.addEventListener('click')
+    selectDifficulty.addEventListener('change', function() {
+        setDifficulty(this.value); // this.value will be 'easy', 'medium', or 'hard'
+    });
 
-//Game Items
-    // Ball properties
-        let ballRadius = 10
-        let ballX = canvas.width / 2
-        let ballY = canvas.height / 2
-        let ballSpeedX = 2.5
-        let ballSpeedY = 2.5
-
-    // Paddle properties
-        let paddleHeight = 100
-        let paddleWidth = 10
-        let leftPaddle = canvas.height / 2 - paddleHeight / 2
-        let rightPaddle = canvas.height / 2 - paddleHeight / 2
-        let paddleSpeed = 7.5
+    // Functions
     
-    // Score Boarding
-    let leftPlayerScore = 0
-    let rightPlayerScore = 0
-    let winningScore = 10
-
-// Functions
-
     //Movement 
-        function keyDownPress(event) {
-            if (event.key === "ArrowUp") {
-                upPressed = true
-            } else if (event.key === "ArrowDown") {
-                downPressed = true
-            } else if (event.key === "w") {
-                wPressed = true
-            }  else if (event.key === "s") {
-                sPressed = true
-            }
+    function keyDownPress(event) {
+        if (event.key === "ArrowUp") {
+            upPressed = true
+        } else if (event.key === "ArrowDown") {
+            downPressed = true
+        } else if (event.key === "w") {
+            wPressed = true
+        }  else if (event.key === "s") {
+            sPressed = true
         }
+    }
+    
+    function keyUpPress(event) {
+        if (event.key === "ArrowUp") {
+            upPressed = false
+        } else if (event.key === "ArrowDown") {
+            downPressed = false
+        } else if (event.key === "w") {
+            wPressed = false
+        }  else if (event.key === "s") {
+            sPressed = false
+        }
+    }
+    
+    // Ball Moving and resetting
+    function Ballreset () {
+        ballX = canvas.width / 2
+        ballY = canvas.height / 2
+        ballSpeedX = -ballSpeedX
+        ballSpeedY = Math.random() *10 - 5
+    }
+    
+    // winningScore
+    function playerWin (player) {
+        if (player === "AI Player") {
+            messageEl.textContent = "Oh NO! Try Again"
+        } else if (player === "Left Player") {
+            messageEl.textContent = "Congrats! You Win!"
+        }
+        gameRunning = false
+        Ballreset()
+    }
 
-        function keyUpPress(event) {
-            if (event.key === "ArrowUp") {
-                upPressed = false
-            } else if (event.key === "ArrowDown") {
-                downPressed = false
-            } else if (event.key === "w") {
-                wPressed = false
-            }  else if (event.key === "s") {
-                sPressed = false
-            }
-        }
-        
-        // Ball Moving and resetting
-        function Ballreset () {
-            ballX = canvas.width / 2
-            ballY = canvas.height / 2
-            ballSpeedX = -ballSpeedX
-            ballSpeedY = Math.random() *10 - 5
-        }
+    //Change Difficulty    
+    function setDifficulty(difficulty) {
+        currentDifficulty = difficultyLevels[difficulty]
+        ballSpeedX = currentDifficulty.ballSpeedX
+        ballSpeedY = currentDifficulty.ballSpeedY
+    }
+    
 
-        // winningScore
-        function playerWin (player) {
-            if (player === "AI Player") {
-                messageEl.textContent = "Oh NO! Try Again"
-            } else if (player === "Left Player") {
-                messageEl.textContent = "Congrats! You Win!"
-            }
-            Ballreset()
-            }
 //Game State of Play
     function update() {
         //moving paddles
-            if ((upPressed || wPressed) && leftPaddle > 0) {
-                leftPaddle -= paddleSpeed
+        if ((upPressed || wPressed) && leftPaddle > 0) {
+            leftPaddle -= paddleSpeed
             } else if ((downPressed || sPressed) && leftPaddle + paddleHeight < canvas.height) {
                 leftPaddle += paddleSpeed
             }
@@ -201,7 +235,10 @@
 
 // Game Loop
 function loop() {
-    update()
-    draw()
-    animationId = requestAnimationFrame(loop)
+    if(gameRunning) {
+        update()
+        draw()
+        animationId = requestAnimationFrame(loop)
+    }
+    
 }      
